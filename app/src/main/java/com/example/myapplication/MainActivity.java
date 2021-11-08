fb7970721c542ea276c0fc3ca24f7b21eaa9e5ca
johnson413011 <73644230+johnson413011@users.noreply.github.com>
2021-11-09 03:18:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void ugly(View view){
+
+            ImageView img = (ImageView)findViewById(R.id.imageView);
+            Bitmap bImage = BitmapFactory.decodeResource(this.getResources(),R.drawable.aa1);
+            img.setImageBitmap(bImage);
+        }
+    }
+
+
